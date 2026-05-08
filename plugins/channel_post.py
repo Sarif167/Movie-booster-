5e697a146c2d7031ee3236edb58344dcd5f6f1d1
Botmaster55 <wajsarif461@gmail.com>
2026-05-08 11:30:59 +0530

    Add files via upload

---
+++ plugins/channel_post.py
@@ -0,0 +1,26 @@
+
+        from pyrogram import Client, filters
+        from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+        from info import CHANNEL_ID, ADMINS
+
+        @Client.on_message(filters.video & filters.user(ADMINS))
+        async def auto_post(client, message):
+
+            caption = f"""
+🎬 New Movie Uploaded
+📁 Quality: 720p
+⭐ IMDb: 8.5
+"""
+
+            buttons = InlineKeyboardMarkup([
+                [InlineKeyboardButton("💳 Buy File", callback_data="buy")]
+            ])
+
+            await client.send_video(
+                CHANNEL_ID,
+                video=message.video.file_id,
+                caption=caption,
+                reply_markup=buttons
+            )
+
+            await message.reply_text("Movie Posted Successfully")
