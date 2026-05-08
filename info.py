daafa5020094eb40be42a6fc9b46a3979c008b4e
Botmaster55 <wajsarif461@gmail.com>
2026-05-08 11:37:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,14 +5,14 @@ API_ID = int(os.environ.get("API_ID", "23621595"))
 API_HASH = os.environ.get("API_HASH", "de904be2b4cd4efe2ea728ded17ca77d")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN")
 
-MONGO_URI = os.environ.get("MONGO_URI", "YOUR_MONGO_URL")
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://technicalseekho249_db_user:JI2rAJvc0RE2asYE@cluster0.8hgdhqt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 ADMINS = [1249672673]
 
 LOG_CHANNEL = -1002580860502
 CHANNEL_ID = -1002133932311
 
-FSUB_CHANNEL = "yourchannelusername"
+FSUB_CHANNEL = "MovieSearchAutoGroup"
 
 UPI_ID = "yourupi@paytm"
 QR_IMAGE = "https://example.com/qr.jpg"
