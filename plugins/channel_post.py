d588c4d76a4c0197332051150d503ff5f8af921d
Botmaster55 <wajsarif461@gmail.com>
2026-05-08 11:48:58 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -1,26 +1,26 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from info import CHANNEL_ID, ADMINS
 
-        from pyrogram import Client, filters
-        from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-        from info import CHANNEL_ID, ADMINS
 
-        @Client.on_message(filters.video & filters.user(ADMINS))
-        async def auto_post(client, message):
+@Client.on_message(filters.video & filters.user(ADMINS))
+async def auto_post(client, message):
 
-            caption = f"""
+    caption = """
 🎬 New Movie Uploaded
 📁 Quality: 720p
 ⭐ IMDb: 8.5
 """
 
-            buttons = InlineKeyboardMarkup([
-                [InlineKeyboardButton("💳 Buy File", callback_data="buy")]
-            ])
+    buttons = InlineKeyboardMarkup([
+        [InlineKeyboardButton("💳 Buy File", callback_data="buy")]
+    ])
 
-            await client.send_video(
-                CHANNEL_ID,
-                video=message.video.file_id,
-                caption=caption,
-                reply_markup=buttons
-            )
+    await client.send_video(
+        CHANNEL_ID,
+        video=message.video.file_id,
+        caption=caption,
+        reply_markup=buttons
+    )
 
-            await message.reply_text("Movie Posted Successfully")
+    await message.reply_text("Movie Posted Successfully")
