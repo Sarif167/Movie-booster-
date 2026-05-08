5c613f57f50364bfd1c43d016d4c2ffce1df3a8e
Botmaster55 <wajsarif461@gmail.com>
2026-05-08 11:35:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,16 +1,16 @@
 
 import os
 
-API_ID = int(os.environ.get("API_ID", "123456"))
-API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH")
+API_ID = int(os.environ.get("API_ID", "23621595"))
+API_HASH = os.environ.get("API_HASH", "de904be2b4cd4efe2ea728ded17ca77d")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN")
 
 MONGO_URI = os.environ.get("MONGO_URI", "YOUR_MONGO_URL")
 
-ADMINS = [123456789]
+ADMINS = [1249672673]
 
-LOG_CHANNEL = -1001234567890
-CHANNEL_ID = -1001234567890
+LOG_CHANNEL = -1002580860502
+CHANNEL_ID = -1002133932311
 
 FSUB_CHANNEL = "yourchannelusername"
 
