e28f69784c8e785e5f160c027ef8c741588aed42
Botmaster55 <wajsarif461@gmail.com>
2026-05-08 12:13:40 +0530

    Refactor video and document posting logic

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -1,89 +1,25 @@
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from info import CHANNEL_ID, ADMINS
+caption = f"""
+🎬 {message.video.file_name}
 
-FILES_DB = {}
-
-@Client.on_message((filters.video | filters.document) & filters.user(ADMINS))
-async def auto_post(client, message):
-
-    media_group_id = message.media_group_id
-
-    if media_group_id:
-
-        if media_group_id not in FILES_DB:
-            FILES_DB[media_group_id] = []
-
-        if message.video:
-            FILES_DB[media_group_id].append(
-                ("video", message.video.file_id)
-            )
-
-        elif message.document:
-            FILES_DB[media_group_id].append(
-                ("document", message.document.file_id)
-            )
-
-        buttons = InlineKeyboardMarkup([
-            [
-                InlineKeyboardButton(
-                    "💳 Buy Full Pack",
-                    callback_data=f"buy_{media_group_id}"
-                )
-            ]
-        ])
-
-        caption = f"""
-🎬 New Movie Pack Uploaded
-
-📦 Total Files: {len(FILES_DB[media_group_id])}
-
-💰 One Payment = All Files
-"""
-
-        if len(FILES_DB[media_group_id]) == 1:
-
-            if message.video:
-                await client.send_video(
-                    CHANNEL_ID,
-                    video=message.video.file_id,
-                    caption=caption,
-                    reply_markup=buttons
-                )
-
-            else:
-                await client.send_document(
-                    CHANNEL_ID,
-                    document=message.document.file_id,
-                    caption=caption,
-                    reply_markup=buttons
-                )
-
-    else:
-
-        buttons = InlineKeyboardMarkup([
-            [InlineKeyboardButton("💳 Buy File", callback_data="buy_single")]
-        ])
-
-        caption = """
-🎬 New Movie Uploaded
+⭐ IMDb: 8.5
 💰 Paid Content
 """
 
-        if message.video:
-            await client.send_video(
-                CHANNEL_ID,
-                video=message.video.file_id,
-                caption=caption,
-                reply_markup=buttons
-            )
+buttons = InlineKeyboardMarkup([
+    [InlineKeyboardButton("💳 Buy File", callback_data="buy")]
+])
 
-        else:
-            await client.send_document(
-                CHANNEL_ID,
-                document=message.document.file_id,
-                caption=caption,
-                reply_markup=buttons
-            )
+thumb = None
 
-    await message.reply_text("Files Added Successfully ✅")
+if message.video and message.video.thumbs:
+    thumb = message.video.thumbs[0].file_id
+
+elif message.document and message.document.thumbs:
+    thumb = message.document.thumbs[0].file_id
+
+await client.send_photo(
+    CHANNEL_ID,
+    photo=thumb,
+    caption=caption,
+    reply_markup=buttons
+)
