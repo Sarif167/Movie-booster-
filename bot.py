41e593430328e96f3108f211ed2787487882994e
Botmaster55 <wajsarif461@gmail.com>
2026-05-08 11:53:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,16 @@
-
+from flask import Flask
+from threading import Thread
 from pyrogram import Client
 from info import API_ID, API_HASH, BOT_TOKEN
+
+web_app = Flask(__name__)
+
+@web_app.route("/")
+def home():
+    return "Bot Running Successfully"
+
+def run_web():
+    web_app.run(host="0.0.0.0", port=8080)
 
 app = Client(
     "PaidFileBot",
@@ -9,6 +19,8 @@ app = Client(
     bot_token=BOT_TOKEN,
     plugins=dict(root="plugins")
 )
+
+Thread(target=run_web).start()
 
 print("Bot Started Successfully")
 app.run()
