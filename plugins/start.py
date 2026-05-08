d9fb6b0bfd35640221307b13b713d5866be225eb
Botmaster55 <wajsarif461@gmail.com>
2026-05-08 11:32:01 +0530

    Add files via upload

---
+++ plugins/start.py
@@ -0,0 +1,15 @@
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from info import FSUB_CHANNEL
+
+@Client.on_message(filters.command("start"))
+async def start_cmd(client, message):
+    buttons = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Join Channel", url=f"https://t.me/{FSUB_CHANNEL}")],
+    ])
+
+    await message.reply_text(
+        "Welcome To Paid File Bot",
+        reply_markup=buttons
+    )
